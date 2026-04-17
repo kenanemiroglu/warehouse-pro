7b101494582e0b4214f15017b8af9402c91ed90a
kenanemiroglu <kenanemiroglu24@gmail.com>
2026-04-17 14:23:28 +0300

    Refactor main.js for window dimensions and path

--- electron/main.js
+++ electron/main.js
@@ -1,12 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1100,
-    height: 800
+    width: 1000,
+    height: 700,
   });
 
-  win.loadFile('renderer/index.html');
+  win.loadFile(path.join(__dirname, "../renderer/index.html"));
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+});
